a16b07d6c3f39857b81b7bef92ae33d94fcec56f
jzy199009 <940429676@qq.com>
2017-03-29 19:27:21 +0800

    第一次提交

---
+++ app/src/main/java/cn/jzy/smartcity/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package cn.jzy.smartcity.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.jzy.smartcity.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
